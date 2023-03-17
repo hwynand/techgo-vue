10bf6e65befcd38abdc43db37117e566547f225e
huongpx <huong.px01@gmail.com>
2023-03-17 15:15:32 +0700

    base project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import DefaultLayout from './layouts/DefaultLayout.vue';
+</script>
+
+<template>
+  <default-layout>
+    <RouterView />
+  </default-layout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="header">
+    <slot name="header"></slot>
+  </div>
+  <div id="main">
+    <slot name="main"></slot>
+  </div>
+  <div id="footer">
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#main {
+  min-height: 90vh;
+}
+</style>
